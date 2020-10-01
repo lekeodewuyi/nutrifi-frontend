53a325970da0458950c926c908199589ae0372f2
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-10-01 05:26:02 -0400

    change: modifies script name

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/newscript.js',
+        app: './src/index.js',
     },
     output: {
         path: path.resolve(__dirname, 'build'),
